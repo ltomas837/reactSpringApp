737f62e6195c2da0bd4aad596362d01cf0034c90
Léo Tomas <ltomas@enseirb-matmeca.fr>
2021-02-20 22:28:04 +0100

    Adding project files

---
+++ online_board_games/src/main/java/com/online_board_games/OnlineBoardGamesApplication.java
@@ -0,0 +1,13 @@
+package com.online_board_games;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineBoardGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineBoardGamesApplication.class, args);
+	}
+
+}
---
+++ online_board_games/src/test/java/com/online_board_games/OnlineBoardGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.online_board_games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineBoardGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
